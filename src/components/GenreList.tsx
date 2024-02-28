8d8d73d75880b41076e1db95011214bd899dc16c
Prathyusha Chinthakunta <chinthakuntaprathyusha2001@gmail.com>
2024-02-28 19:13:43 +0530

    Display genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,15 +1,26 @@
 import React from "react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import useData from "../hooks/useData";
-
+import { Image, List, ListItem } from "@chakra-ui/react";
+import { HStack } from "@chakra-ui/react";
+import getCroppedImageUrl from "../image-url";
 const GenreList = () => {
   const { data } = useGenres();
   return (
-    <ul>
+    <List>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <text fontSize="lg">{genre.name}</text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
